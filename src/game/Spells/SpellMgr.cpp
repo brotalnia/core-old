9c2083880e0ed26f9bc0844a32e3a120ef1eb41f
brotalnia <brotalnia@gmail.com>
2018-07-21 20:44:01 +0300

    Fix Furious Howl..

--- src/game/Spells/SpellMgr.cpp
+++ src/game/Spells/SpellMgr.cpp
@@ -4477,6 +4477,10 @@ void SpellMgr::LoadSpells()
         //spell->RequiredAuraVision = fields[172].GetUInt32();
         spell->Custom = fields[173].GetUInt32();
 
+        // Furios Howl
+        if ((spell->SpellIconID == 1573) && (spell->SpellVisual == 7299) && !(spell->AuraInterruptFlags & AURA_INTERRUPT_FLAG_MELEE_ATTACK))
+            spell->AuraInterruptFlags |= AURA_INTERRUPT_FLAG_MELEE_ATTACK;
+
         spell->InitCachedValues();
         mSpellEntryMap[spellId] = std::move(spell);
 
