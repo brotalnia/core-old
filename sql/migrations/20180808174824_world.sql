0d2f9358ff3b4b8091b8b3095fb69ac7934271ad
brotalnia <brotalnia@gmail.com>
2018-08-09 14:45:52 +0300

    Try to fix guard summon loop.

--- sql/migrations/20180808174824_world.sql
+++ sql/migrations/20180808174824_world.sql
@@ -24,6 +24,26 @@ UPDATE `creature_template` SET `flags_extra`=0 WHERE `entry`=15341;
 
 UPDATE `game_event` SET `start_time`='2015-03-30 04:00:00', `disabled`=0, `patch_max`=1 WHERE `entry`=157;
 
+-- Seeker Thompson
+DELETE FROM `creature_ai_events` WHERE `creature_id`=14404;
+INSERT INTO `creature_ai_events` (`id`, `creature_id`, `condition_id`, `event_type`, `event_inverse_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `action1_script`, `action2_script`, `action3_script`, `comment`) VALUES (1440401, 14404, 0, 4, 2, 100, 0, 0, 0, 0, 0, 1440401, 0, 0, 'Seeker Thompson - Summon Deathguard Elite on Aggro');
+INSERT INTO `creature_ai_events` (`id`, `creature_id`, `condition_id`, `event_type`, `event_inverse_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `action1_script`, `action2_script`, `action3_script`, `comment`) VALUES (1440402, 14404, 0, 11, 0, 100, 0, 0, 0, 0, 0, 1440402, 0, 0, 'Seeker Thompson - Set Phase to 0 on Spawn');
+DELETE FROM `creature_ai_scripts` WHERE `id`=1440401;
+INSERT INTO `creature_ai_scripts` (`id`, `delay`, `command`, `datalong`, `datalong2`, `datalong3`, `datalong4`, `target_param1`, `target_param2`, `target_type`, `data_flags`, `dataint`, `dataint2`, `dataint3`, `dataint4`, `x`, `y`, `z`, `o`, `condition_id`, `comments`) VALUES (1440401, 0, 10, 7980, 30000, 3, 30, 0, 0, 0, 0, 8, 0, 1, 1, 0, 0, 0, 0, 0, 'Seeker Thompson - Summon Creature Deathguard Elite');
+INSERT INTO `creature_ai_scripts` (`id`, `delay`, `command`, `datalong`, `datalong2`, `datalong3`, `datalong4`, `target_param1`, `target_param2`, `target_type`, `data_flags`, `dataint`, `dataint2`, `dataint3`, `dataint4`, `x`, `y`, `z`, `o`, `condition_id`, `comments`) VALUES (1440401, 0, 44, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 'Seeker Thompson - Set Phase to 1');
+DELETE FROM `creature_ai_scripts` WHERE `id`=1440402;
+INSERT INTO `creature_ai_scripts` (`id`, `delay`, `command`, `datalong`, `datalong2`, `datalong3`, `datalong4`, `target_param1`, `target_param2`, `target_type`, `data_flags`, `dataint`, `dataint2`, `dataint3`, `dataint4`, `x`, `y`, `z`, `o`, `condition_id`, `comments`) VALUES (1440402, 0, 44, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 'Seeker Thompson - Set Phase to 0');
+
+-- Seeker Nahr
+DELETE FROM `creature_ai_events` WHERE `creature_id`=14403;
+INSERT INTO `creature_ai_events` (`id`, `creature_id`, `condition_id`, `event_type`, `event_inverse_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `action1_script`, `action2_script`, `action3_script`, `comment`) VALUES (1440301, 14403, 0, 4, 2, 100, 0, 0, 0, 0, 0, 1440301, 0, 0, 'Seeker Nahr - Summon Deathguard Elite on Aggro');
+INSERT INTO `creature_ai_events` (`id`, `creature_id`, `condition_id`, `event_type`, `event_inverse_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `action1_script`, `action2_script`, `action3_script`, `comment`) VALUES (1440302, 14403, 0, 11, 0, 100, 0, 0, 0, 0, 0, 1440302, 0, 0, 'Seeker Nahr - Set Phase to 0 on Spawn');
+DELETE FROM `creature_ai_scripts` WHERE `id`=1440301;
+INSERT INTO `creature_ai_scripts` (`id`, `delay`, `command`, `datalong`, `datalong2`, `datalong3`, `datalong4`, `target_param1`, `target_param2`, `target_type`, `data_flags`, `dataint`, `dataint2`, `dataint3`, `dataint4`, `x`, `y`, `z`, `o`, `condition_id`, `comments`) VALUES (1440301, 0, 10, 7980, 30000, 3, 30, 0, 0, 0, 0, 8, 0, 1, 1, 0, 0, 0, 0, 0, 'Seeker Nahr - Summon Creature Deathguard Elite');
+INSERT INTO `creature_ai_scripts` (`id`, `delay`, `command`, `datalong`, `datalong2`, `datalong3`, `datalong4`, `target_param1`, `target_param2`, `target_type`, `data_flags`, `dataint`, `dataint2`, `dataint3`, `dataint4`, `x`, `y`, `z`, `o`, `condition_id`, `comments`) VALUES (1440301, 0, 44, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 'Seeker Nahr - Set Phase to 1');
+DELETE FROM `creature_ai_scripts` WHERE `id`=1440302;
+INSERT INTO `creature_ai_scripts` (`id`, `delay`, `command`, `datalong`, `datalong2`, `datalong3`, `datalong4`, `target_param1`, `target_param2`, `target_type`, `data_flags`, `dataint`, `dataint2`, `dataint3`, `dataint4`, `x`, `y`, `z`, `o`, `condition_id`, `comments`) VALUES (1440302, 0, 44, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 'Seeker Nahr - Set Phase to 0');
+
 
 -- End of migration.
 END IF;
